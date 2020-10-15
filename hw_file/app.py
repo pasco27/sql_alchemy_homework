5ade9ffe34c980baa14bc1488f7ec0f287281e01
Rob Pascarella <robpascarella@Robs-iMac.local>
2020-10-14 21:18:08 -0500

    after tutor session and working through tutor notes

--- hw_file/app.py
+++ hw_file/app.py
@@ -61,23 +61,27 @@ def precipitation():
 
     # create the dict that will be jsonified of date and prcp
     hawaii_measurements = []
-    for dates in precip:
+    for row in precip:
         measurements = {}
-        measurement['date'] = measurement.date
-        measurement['precipitaion'] = measurement.prcp
-        hawaii_measurements.append(measurements)   
     
+       # i had this below, tutor pointed out that the directtions stated that the 'date' should be the KEY
+       # that makes sense to me, but the below code is cleaner -- keeping it per directions as 'date'= KEY
+       # measurement['date'] = measurement.date
+       # measurement['precipitaion'] = measurement.prcp
+       # hawaii_measurements.append(measurements)   
     
+        measurements[row.date]= row.prcp
+        hawaii_measurements.append(measurements) 
+
+    return jsonify(hawaii_measurements)
+
     # all_locs_prcp = list(np.ravel(results))
     # alternative with list comprehension
     # all_names = [result[0] for result in results]
 
-    return jsonify(hawaii_measurements)
-
 
 @app.route('/api/v1.0/stations')
 def stations():
-
     # Return a JSON list of stations from the dataset 
     
     # Query the database
@@ -85,9 +89,9 @@ def stations():
     station_count = session.query(measurement.station).group_by(measurement.station).all()
     # close session
     session.close()
-   
-    # ???
+
     return jsonify(station_count)
+
 
 
 @app.route('/api/v1.0/tobs')
@@ -97,63 +101,78 @@ def tobs():
     
     # Query the database
     session = Session(engine)
-    ## This was for MIN, from the last exercise 
-    lowest2 = session.query(measurement.station, func.min(measurement.tobs).label('min')).filter(measurement.station=='USC00519281').all()
-    past_year = session.query(measurement.date, measurement.prcp).filter(measurement.date >= "2016-08-23").all()
+    dates_and_temps = session.query(measurement.date, measurement.prcp).filter(measurement.station == 'USC00519281').all()
+
+    #These were from MIN, and latest year ... from jupyter notebook
+    #lowest2 = session.query(measurement.station, func.min(measurement.tobs).label('min')).filter(measurement.station=='USC00519281').all()
+    #past_year = session.query(measurement.date, measurement.prcp).filter(measurement.date >= "2016-08-23").all()
     # close session
     session.close()    
+
+    tobs_results = list(np.ravel(dates_and_temps))
     
-    return jsonify(...whatever I decide to call this...)
+    return jsonify(dates_and_temps)
 
 
-
-
-
-
-
-@app.route('/api/v1.0/start')
-def start():
-    # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start date
+# @app.route('/api/v1.0/<start>')
+# def start():
+#     # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start date
     
-    # Query the database
-    session = Session(engine)
-    #############station_count = session.query(measurement.station).group_by(measurement.station).all()
-    # close session
-    session.close()
+#     # Query the database
+#     session = Session(engine)
+
+#     # close session
+#     session.close()
     
-    return (
-        some stuff....
-    )
+#     return jsonifty(xyz)
 
 
+@app.route('/api/v1.0/temps/<start>')
 @app.route('/api/v1.0/temps/<start>/<end>')
-def start_end(start, end):
+def start_end(start = None, end = None):
     """Returning min, max, and average temp for this range
         if available, else 404 error message if range unavailable"""
 
+# Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
+# When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than and equal to the start date.
+# When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates between the start and end date inclusive
+
     session = Session(engine)
 
-    lowest2 = session.query(measurement.station, func.min(measurement.tobs).label('min')).filter(measurement.station).all()
-    highest2 = session.query(measurement.station, func.max(measurement.tobs).label('max')).filter(measurement.station).all()
-    average2 = session.query(measurement.station, func.avg(measurement.tobs).label('avg')).filter(measurement.station).all()
+    if end:
+            #find the temps in the range
+            #one filters on both 
+    # SELECT  * FROM... 
+    # WHERE
+
+        lo    = session.query(func.min(measurement.tobs).label('min'), func.max(measurement.tobs).label('max'), func.avg(measurement.tobs).label('avg')
+
+
+        lowest2 = session.query(measurement.station, func.min(measurement.tobs).label('min')).filter(measurement.station).all()
+        highest2 = session.query(measurement.station, func.max(measurement.tobs).label('max')).filter(measurement.station).all()
+        average2 = session.query(measurement.station, func.avg(measurement.tobs).label('avg')).filter(measurement.station).all()
+
+    else:
+        lowest2 = session.query(measurement.station, func.min(measurement.tobs).label('min')).filter(measurement.station).all()
+        highest2 = session.query(measurement.station, func.max(measurement.tobs).label('max')).filter(measurement.station).all()
+        average2 = session.query(measurement.station, func.avg(measurement.tobs).label('avg')).filter(measurement.station).all()
 
     # close session
     session.close()
 
 
-
     # do I even need this? it was in the example...
-    search_terms = start.replace(" ", "").lower()
+    # search_terms = start.replace(" ", "").lower()
 
 
-    for temp in start_end_temps:
+    # for temp in start_end_temps:
 
     # loop over the sqlite file, and if a date range is 
-        if end:
-            #find the temps in the range
+    # if end:
+    #         #find the temps in the range
 
-        elseif:
-            #find the temps starting at this date      
+    # else:
+    #         #find the temps starting at this date      
 
     return jsonify(start_end_temps)
 
